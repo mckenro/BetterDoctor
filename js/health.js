d41f1ca1ca3c272e3bbb2b23dca050b7099da4a8
Rob McKenzie <mckenro@gmail.com>
2017-04-21 18:10:41 -0700

    add final project

--- js/health.js
+++ js/health.js
@@ -10,27 +10,14 @@ Health.prototype.getDoctor = function(condition){
       //loop through profile
       for (i = 0; i<response.data.length; i++) {
         //profile photo
-        $('#result').append("<div class='doc-section'>" + "<img class='profile-pic' src=" + response.data[i].profile.image_url + "></div>");
+        $('#result').append("<div class='profile-card'>" + "<img class='profile-pic' src=" + response.data[i].profile.image_url + ">" + "<div class='profile-info'>" + "<h3>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h3>" + "<h5>" + response.data[i].profile.title + "</h5>" + "<p>" + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip + "</p>" + "<p class='space'>phone: " + "<span class='phone'>" + response.data[i].practices[0].phones[0].number + "</span></p>" +"</div>" + "</div>");
 
         //name
-        $('#result').append("<div class='profile-info'>" + "<h3>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h3>" + "<h5>" + response.data[i].profile.title + "</h5>" + "</div>");
+        // $('#result').append("<div class='profile-info'>" + "<h3>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h3>" + "<h5>" + response.data[i].profile.title + "</h5>" + "</div>");
         // console.log(response.data[i].practices[0].phones[0].number);
 
         //contact
-        $('#result').append("<div class='contact'>" + "<p>" + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip + "</p>" + "<p class='space'>phone: " + "<span class='phone'>" + response.data[i].practices[0].phones[0].number + "</span></p></div>");
-
-
-        //loop through specialties
-        // for (i=0; i<response.data[i].specialties.length; i++) {
-        //   console.log(speicalties.name);
-        // }
-      }
-
-      //loop through practices
-      for (i = 0; i<response.data.length; i++) {
-        //address
-        $('#result').append("<div class='address'>" + "<p>" + response.data[i].practices.name + "</p>" + "</div>");
-
+        // $('#result').append("<div class='contact'>" + "<p>" + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip + "</p>" + "<p class='space'>phone: " + "<span class='phone'>" + response.data[i].practices[0].phones[0].number + "</span></p></div>");
 
       }
 
