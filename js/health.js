caae0be2c9a9b83d1a017bf9a5d4aecb3150f7ba
Rob McKenzie <mckenro@gmail.com>
2017-04-22 08:29:37 -0700

    remove excess code

--- js/health.js
+++ js/health.js
@@ -7,24 +7,12 @@ Health.prototype.getDoctor = function(condition){
 
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ condition+'&location=47.6204%2C-122.3508%2C%205&user_location=47.6204%2C-122.3493&skip=0&limit=100&user_key=' + apiKey)
     .then(function(response) {
-      //loop through profile
       for (i = 0; i<response.data.length; i++) {
-        //profile photo
         $('#result').append("<div class='profile-card'>" + "<img class='profile-pic' src=" + response.data[i].profile.image_url + ">" + "<div class='profile-info'>" + "<h3>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h3>" + "<h5>" + response.data[i].profile.title + "</h5>" + "<p>" + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip + "</p>" + "<p class='space'>phone: " + "<span class='phone'>" + response.data[i].practices[0].phones[0].number + "</span></p>" +"</div>" + "</div>");
-
-        //name
-        // $('#result').append("<div class='profile-info'>" + "<h3>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h3>" + "<h5>" + response.data[i].profile.title + "</h5>" + "</div>");
-        // console.log(response.data[i].practices[0].phones[0].number);
-
-        //contact
-        // $('#result').append("<div class='contact'>" + "<p>" + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip + "</p>" + "<p class='space'>phone: " + "<span class='phone'>" + response.data[i].practices[0].phones[0].number + "</span></p></div>");
-
       }
 
-    })
-
-    .fail(function(response) {
-      $('#result').append(error.responseJSON.message);
+    }).fail(function(error) {
+      $('#result').text("Sorry, we couldn't find a match for that search term.");
     });
 
 };
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -5,11 +5,6 @@ $(document).ready(function(){
   $('form#health-form').submit(function(event){
     event.preventDefault();
     var condition = $('input#condition').val();
-    // var location = $('input#location').val();
-    // var zipcode = parseInt(location);
-    var error = "Sorry, we couldn't find a good match.";
     healthObject.getDoctor(condition);
   });
 });
-
-// https://api.betterdoctor.com/2016-03-01/doctors?query=dermatology&location=98101&skip=0&limit=100&user_key=5347729089b5253ce372a9693a75cd33
